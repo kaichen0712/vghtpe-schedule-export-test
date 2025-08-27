0328bbc9c2302e15859349c4383cff95438c5032
詹彥杰 <jack6796@gmail.com>
2025-08-27 11:30:11 +0800

    fix: update lockfile and configs

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/vghtpe-schedule-export-zyj/', // ← 改成你的 repo 名稱
-  build: {
-    outDir: 'dist' // 預設就是 dist，保險寫上
-  }
+  base: '/vghtpe-schedule-export-zyj/',
+  build: { outDir: 'dist' }
 })
