6a0e87c439b211269264d620c55a2998ffca117f
詹彥杰 <jack6796@gmail.com>
2025-08-27 11:05:13 +0800

    Fix GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/vghtpe-schedule-export-zyj/",
-});
+  plugins: [react()],
+  base: '/vghtpe-schedule-export-zyj/', // ← 改成你的 repo 名稱
+  build: {
+    outDir: 'dist' // 預設就是 dist，保險寫上
+  }
+})
